4f7ab22ffaffa8125b40f2efc0ca55b087c08a45
Nilesh Bhosale <nileshnetajibhosale@gmail.com>
2021-01-09 12:14:39 +0000

    Final output

--- starter_files/benchmark.sh
+++ starter_files/benchmark.sh
@@ -70,4 +70,4 @@
 # Total:        114  160  28.0    172     199#
 
 
- ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer REPLACE_WITH_KEY' http://REPLACE_WITH_API_URL/score
+ ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer OWuPftFFjoK26rHsx7vIHdRB4kb7RiTA' http://6b863d65-025f-4cd2-823e-6cf02e85d7f6.southcentralus.azurecontainer.io/score
